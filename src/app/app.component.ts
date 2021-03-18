3c59414f53337cfaf0647e61eb58a5fb26e9c7fc
MSoleti <madhu.soleti@qentelli.com>
2021-03-18 10:58:07 +0530

    valid git hub url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username:string =" Madhu Soleti..!"
-  repoUrl:string="https://github.com/MSoleti";
+  repoUrl:string="https://github.com/MSoleti/Angular";
 }
